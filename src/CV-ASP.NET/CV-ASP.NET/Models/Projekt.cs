6c60d1de57b1464c043329229d7ddf95faa49c52
amandaperbro <amanda.perbro@hotmail.com>
2024-12-20 11:26:29 +0100

    projekt

---
+++ src/CV-ASP.NET/CV-ASP.NET/Models/Projekt.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace CV_ASP.NET.Models
+{
+    public class Projekt
+    {
+        public int Pid { get; set; }
+
+        [Required(ErrorMessage = "Du måste ange ett namn på projektet")]
+        [RegularExpression(@"^[a-zA-Z0-9._-]{3,20}$", ErrorMessage = "Får endast innehålla bokstäver och siffror")]
+        public string? Namn { get; set; }
+
+        [Required(ErrorMessage = "Du måste ange en beskrivning på projektet")]
+        [RegularExpression(@"^[a-zA-Z0-9._-]{3,20}$", ErrorMessage = "Får endast innehålla bokstäver och siffror")]
+        public string? Beskrivning { get; set; }
+        public string? SkapadAv { get; set; }
+        public DateOnly DatumSkapad { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+
+
+        [ForeignKey(nameof(SkapadAv))]
+        [XmlIgnore]
+        public virtual Anvandare Anvandare { get; set; }
+
+        [XmlIgnore]
+        public virtual IEnumerable<AnvProjekt> AnvProjekt { get; set; } = new List<AnvProjekt>();
+
+
+    }
+}
---
+++ src/CV-ASP.NET/CV-ASP.NET/Models/ViewModels/Match.cs
@@ -0,0 +1,11 @@
+﻿namespace CV_ASP.NET.Models.ViewModels
+{
+    public class Match
+    {
+        public Anvandare Anvandare { get; set; }
+        public IEnumerable<Erfarenhet> Erfarenheter { get; set; }
+
+        public IEnumerable<Kompetenser> Kompetenser { get; set; }
+        public CV Cv { get; set; }
+    }
+}
