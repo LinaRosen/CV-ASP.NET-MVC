a71d95f7374befeb699fea21757d4365355de4a8
amandaperbro <amanda.perbro@hotmail.com>
2024-12-27 20:06:16 +0100

    fixa småfel

--- src/CV-ASP.NET/CV-ASP.NET/DataContext/TestDataContext.cs
+++ src/CV-ASP.NET/CV-ASP.NET/DataContext/TestDataContext.cs
@@ -9,7 +9,7 @@ namespace CV_ASP.NET.DataContext
 
         public DbSet<Adress> Adresser { get; set; }
         public DbSet<Anvandare> Anvandare { get; set; }
-        public DbSet<CV> CVs { get; set; }
+        public DbSet<CV> CV { get; set; }
         public DbSet<CV_Erfarenhet> CV_Erfarenhet { get; set; }
         public DbSet<CV_kompetenser> CV_Kompetenser { get; set; }
         public DbSet<CV_Utbildning> CV_Utbildning { get; set; }
@@ -17,21 +17,21 @@ namespace CV_ASP.NET.DataContext
         public DbSet<Kompetenser> Kompetenser { get; set; }
         public DbSet<Meddelande> Meddelande { get; set; }
         public DbSet<Utbildning> Utbildning { get; set; }
-     
+
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Meddelande>()
-                .HasOne(m => m.Frananvandare) 
-                .WithMany(u => u.skickatMeddelande)            
-                .HasForeignKey(m => m.FranAnvandareId)    
+                .HasOne(m => m.Frananvandare)
+                .WithMany(u => u.skickatMeddelande)
+                .HasForeignKey(m => m.FranAnvandareId)
                 .OnDelete(DeleteBehavior.Restrict);
 
             modelBuilder.Entity<Meddelande>()
-                .HasOne(m => m.Tillanvandare) 
-                .WithMany(u => u.TagitEmotMeddelande)            
-                .HasForeignKey(m => m.TillAnvandareId)    
+                .HasOne(m => m.Tillanvandare)
+                .WithMany(u => u.TagitEmotMeddelande)
+                .HasForeignKey(m => m.TillAnvandareId)
                 .OnDelete(DeleteBehavior.Restrict);
 
             base.OnModelCreating(modelBuilder);
@@ -44,42 +44,6 @@ namespace CV_ASP.NET.DataContext
             modelBuilder.Entity<AnvProjekt>()
                 .HasKey(up => new { up.Anvid, up.Pid });
 
-            modelBuilder.Entity<Anvandare>().HasData(
-            new Anvandare
-            {
-                Id = "1", // IdentityUser använder Id som primärnyckel
-                UserName = "LinaRos",
-                Anvandarnamn = "LinaRos",
-                Fornamn = "Lina",
-                Efternamn = "Rosén",
-                Email = "lina@example.com",
-                NormalizedUserName = "LINAROS",
-                NormalizedEmail = "LINA@EXAMPLE.COM"
-            },
-            new Anvandare
-            {
-                Id = "2",
-                UserName = "NoraBolin",
-                Anvandarnamn = "NoraBolin",
-                Fornamn = "Nora",
-                Efternamn = "Bolin",
-                Email = "nora@example.com",
-                NormalizedUserName = "NORABOLIN",
-                NormalizedEmail = "NORA@EXAMPLE.COM"
-            },
-            new Anvandare
-            {
-                Id = "3",
-                UserName = "AmandaPerbro",
-                Anvandarnamn = "AmandaPerbro",
-                Fornamn = "Amanda",
-                Efternamn = "Perbro",
-                Email = "amanda@example.com",
-                NormalizedUserName = "AMANDAPERBRO",
-                NormalizedEmail = "AMANDA@EXAMPLE.COM"
-
-            }
-            );
         }
 
        
---
+++ src/CV-ASP.NET/CV-ASP.NET/Migrations/20241227190354_test4.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CV_ASP.NET.Migrations
+{
+    /// <inheritdoc />
+    public partial class test4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Anvandare",
+                keyColumn: "Id",
+                keyValue: "1");
+
+            migrationBuilder.DeleteData(
+                table: "Anvandare",
+                keyColumn: "Id",
+                keyValue: "2");
+
+            migrationBuilder.DeleteData(
+                table: "Anvandare",
+                keyColumn: "Id",
+                keyValue: "3");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Anvandare",
+                columns: new[] { "Id", "AccessFailedCount", "Anvandarnamn", "ConcurrencyStamp", "Efternamn", "Email", "EmailConfirmed", "Fornamn", "ListadStartsida", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "PrivatProfil", "Profilbild", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[,]
+                {
+                    { "1", 0, "LinaRos", "b29d929a-4114-403b-8783-d4c7b5615a8b", "Rosén", "lina@example.com", false, "Lina", false, false, null, "LINA@EXAMPLE.COM", "LINAROS", null, null, false, false, null, "5968e922-29f1-4b51-af68-de02baad00b5", false, "LinaRos" },
+                    { "2", 0, "NoraBolin", "50bd6cf8-ac91-4d2d-8714-acd293fec102", "Bolin", "nora@example.com", false, "Nora", false, false, null, "NORA@EXAMPLE.COM", "NORABOLIN", null, null, false, false, null, "de9c93ea-d3e4-4326-af8e-a8a96125e02c", false, "NoraBolin" },
+                    { "3", 0, "AmandaPerbro", "a444b5e0-5cbb-4cad-804e-172753d3c2f4", "Perbro", "amanda@example.com", false, "Amanda", false, false, null, "AMANDA@EXAMPLE.COM", "AMANDAPERBRO", null, null, false, false, null, "ee32c93e-3f3d-411e-9547-3b7aac6c5f57", false, "AmandaPerbro" }
+                });
+        }
+    }
+}
