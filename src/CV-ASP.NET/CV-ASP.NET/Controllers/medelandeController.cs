0873dff124c810b5cc4b6a782d813a9e7896cccd
norab <norabolin@hotmail.com>
2024-12-30 21:44:16 +0100

    medelande

---
+++ src/CV-ASP.NET/CV-ASP.NET/Controllers/medelandeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CV_ASP.NET.Controllers
+{
+    public class MedelandeController:Controller
+    {
+        public IActionResult MedelandeSida()
+        {
+            return View();
+                
+        }
+    }
+}
