788519288fc629c94b8d9fa8a26c9295a4514e8f
LinaRosen <linros0102@gmail.com>
2025-01-11 18:25:26 +0100

    Ändra sitt lösenord

---
+++ src/CV-ASP.NET/CV-ASP.NET/Models/ViewModels/AndraLosenordViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CV_ASP.NET.Models.ViewModels
+{
+    public class AndraLosenordViewModel
+    {
+        [Required(ErrorMessage = "Du måste ange ditt nuvarande lösenord")]
+        public string NuvarandeLosenord { get; set; }
+
+        [Required(ErrorMessage = "Du måste ange ett nytt lösenord")]
+        [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "Lösenordet måste vara minst 6 tecken långt")]
+        public string NyttLosenord { get; set; }
+
+        [Required(ErrorMessage = "Du måste bekräfta ditt nya lösenord")]
+        [Compare("NyttLosenord", ErrorMessage = "Lösenorden matchar inte")]
+        public string BekraftaNyttLosenord { get; set; }
+    }
+}
+
